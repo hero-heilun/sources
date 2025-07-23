a6d0638fe4ffccf30ef00900f3776bd93857f426
JMcrafter26 <77780772+JMcrafter26@users.noreply.github.com>
2025-07-23 18:06:36 +0200

    Upgraded to latest version of Global Extractor - Filemoon should now work

--- fireanime/v2/FireAnimeEngSub.js
+++ fireanime/v2/FireAnimeEngSub.js
@@ -190,7 +190,7 @@ async function sendLog(message) {
 // EDITING THIS FILE COULD BREAK THE UPDATER AND CAUSE ISSUES WITH THE EXTRACTOR
 
 /* {GE START} */
-/* {VERSION: 1.1.2} */
+/* {VERSION: 1.1.3} */
 
 /**
  * @name global_extractor.js
@@ -198,8 +198,8 @@ async function sendLog(message) {
  * @author Cufiy
  * @url https://github.com/JMcrafter26/sora-global-extractor
  * @license CUSTOM LICENSE - see https://github.com/JMcrafter26/sora-global-extractor/blob/main/LICENSE
- * @date 2025-07-22 23:05:55
- * @version 1.1.2
+ * @date 2025-07-23 17:47:48
+ * @version 1.1.3
  * @note This file was generated automatically.
  * The global extractor comes with an auto-updating feature, so you can always get the latest version. https://github.com/JMcrafter26/sora-global-extractor#-auto-updater
  */
@@ -383,6 +383,13 @@ async function extractStreamUrlByProvider(url, provider) {
          console.log("Error extracting stream URL from bigwarp:", error);
          return null;
       }
+    case "doodstream":
+      try {
+         return await doodstreamExtractor(html, url);
+      } catch (error) {
+         console.log("Error extracting stream URL from doodstream:", error);
+         return null;
+      }
     case "filemoon":
       try {
          return await filemoonExtractor(html, url);
@@ -424,31 +431,6 @@ async function extractStreamUrlByProvider(url, provider) {
   }
 }
 
-/**
- * Uses Sora's fetchv2 on ipad, fallbacks to regular fetch on Windows
- * @author ShadeOfChaos
- *
- * @param {string} url The URL to make the request to.
- * @param {object} [options] The options to use for the request.
- * @param {object} [options.headers] The headers to send with the request.
- * @param {string} [options.method='GET'] The method to use for the request.
- * @param {string} [options.body=null] The body of the request.
- *
- * @returns {Promise<Response|null>} The response from the server, or null if the
- * request failed.
- */
-async function soraFetch(url, options = { headers: {}, method: 'GET', body: null }) {
-    try {
-        return await fetchv2(url, options.headers ?? {}, options.method ?? 'GET', options.body ?? null);
-    } catch(e) {
-        try {
-            return await fetch(url, options);
-        } catch(error) {
-            await console.log('soraFetch error: ' + error.message);
-            return null;
-        }
-    }
-}
 
 ////////////////////////////////////////////////
 //                 EXTRACTORS                 //
@@ -472,8 +454,42 @@ async function bigwarpExtractor(videoPage, url = null) {
   console.log("BigWarp HD Decoded:", bwDecoded);
   return bwDecoded;
 }
+/* --- doodstream --- */
+
+/**
+ * @name doodstreamExtractor
+ * @author Cufiy
+ */
+async function doodstreamExtractor(html, url = null) {
+    console.log("DoodStream extractor called");
+    console.log("DoodStream extractor URL: " + url);
+        const streamDomain = url.match(/https:\/\/(.*?)\//, url)[0].slice(8, -1);
+        const md5Path = html.match(/'\/pass_md5\/(.*?)',/, url)[0].slice(11, -2);
+        const token = md5Path.substring(md5Path.lastIndexOf("/") + 1);
+        const expiryTimestamp = new Date().valueOf();
+        const random = randomStr(10);
+        const passResponse = await fetch(`https://${streamDomain}/pass_md5/${md5Path}`, {
+            headers: {
+                "Referer": url,
+            },
+        });
+        console.log("DoodStream extractor response: " + passResponse.status);
+        const responseData = await passResponse.text();
+        const videoUrl = `${responseData}${random}?token=${token}&expiry=${expiryTimestamp}`;
+        console.log("DoodStream extractor video URL: " + videoUrl);
+        return videoUrl;
+}
+function randomStr(length) {
+    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let result = "";
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return result;
+}
 /* --- filemoon --- */
 
+/* {REQUIRED PLUGINS: unbaser} */
 /**
  * @name filemoonExtractor
  * @author Cufiy - Inspired by Churly
@@ -489,6 +505,7 @@ async function filemoonExtractor(html, url = null) {
             headers: {
                 "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
                 "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+                "Referer": url,
             }
         });
         console.log("Iframe Response: " + iframeResponse.status);
@@ -522,6 +539,7 @@ async function filemoonExtractor(html, url = null) {
         return null;
     }
 }
+
 
 /* --- mp4upload --- */
 
@@ -689,5 +707,127 @@ function voeShiftChars(str, shift) {
     .join("");
 }
 
+////////////////////////////////////////////////
+//                 PLUGINS                    //
+////////////////////////////////////////////////
+
+/**
+ * Uses Sora's fetchv2 on ipad, fallbacks to regular fetch on Windows
+ * @author ShadeOfChaos
+ *
+ * @param {string} url The URL to make the request to.
+ * @param {object} [options] The options to use for the request.
+ * @param {object} [options.headers] The headers to send with the request.
+ * @param {string} [options.method='GET'] The method to use for the request.
+ * @param {string} [options.body=null] The body of the request.
+ *
+ * @returns {Promise<Response|null>} The response from the server, or null if the
+ * request failed.
+ */
+async function soraFetch(url, options = { headers: {}, method: 'GET', body: null }) {
+    try {
+        return await fetchv2(url, options.headers ?? {}, options.method ?? 'GET', options.body ?? null);
+    } catch(e) {
+        try {
+            return await fetch(url, options);
+        } catch(error) {
+            await console.log('soraFetch error: ' + error.message);
+            return null;
+        }
+    }
+}
+
+class Unbaser {
+    constructor(base) {
+        this.ALPHABET = {
+            62: "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+            95: "' !\"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~'",
+        };
+        this.dictionary = {};
+        this.base = base;
+        if (36 < base && base < 62) {
+            this.ALPHABET[base] = this.ALPHABET[base] ||
+                this.ALPHABET[62].substr(0, base);
+        }
+        if (2 <= base && base <= 36) {
+            this.unbase = (value) => parseInt(value, base);
+        }
+        else {
+            try {
+                [...this.ALPHABET[base]].forEach((cipher, index) => {
+                    this.dictionary[cipher] = index;
+                });
+            }
+            catch (er) {
+                throw Error("Unsupported base encoding.");
+            }
+            this.unbase = this._dictunbaser;
+        }
+    }
+    _dictunbaser(value) {
+        let ret = 0;
+        [...value].reverse().forEach((cipher, index) => {
+            ret = ret + ((Math.pow(this.base, index)) * this.dictionary[cipher]);
+        });
+        return ret;
+    }
+}
+function unpack(source) {
+    let { payload, symtab, radix, count } = _filterargs(source);
+    if (count != symtab.length) {
+        throw Error("Malformed p.a.c.k.e.r. symtab.");
+    }
+    let unbase;
+    try {
+        unbase = new Unbaser(radix);
+    }
+    catch (e) {
+        throw Error("Unknown p.a.c.k.e.r. encoding.");
+    }
+    function lookup(match) {
+        const word = match;
+        let word2;
+        if (radix == 1) {
+            word2 = symtab[parseInt(word)];
+        }
+        else {
+            word2 = symtab[unbase.unbase(word)];
+        }
+        return word2 || word;
+    }
+    source = payload.replace(/\b\w+\b/g, lookup);
+    return _replacestrings(source);
+    function _filterargs(source) {
+        const juicers = [
+            /}\('(.*)', *(\d+|\[\]), *(\d+), *'(.*)'\.split\('\|'\), *(\d+), *(.*)\)\)/,
+            /}\('(.*)', *(\d+|\[\]), *(\d+), *'(.*)'\.split\('\|'\)/,
+        ];
+        for (const juicer of juicers) {
+            const args = juicer.exec(source);
+            if (args) {
+                let a = args;
+                if (a[2] == "[]") {
+                }
+                try {
+                    return {
+                        payload: a[1],
+                        symtab: a[4].split("|"),
+                        radix: parseInt(a[2]),
+                        count: parseInt(a[3]),
+                    };
+                }
+                catch (ValueError) {
+                    throw Error("Corrupted p.a.c.k.e.r. data.");
+                }
+            }
+        }
+        throw Error("Could not make sense of p.a.c.k.e.r data (unexpected code structure)");
+    }
+    function _replacestrings(source) {
+        return source;
+    }
+}
+
+ 
 
 /* {GE END} */
